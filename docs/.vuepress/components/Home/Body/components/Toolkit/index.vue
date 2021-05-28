5fd92b8344c9fcbd2db85669bbab36bc025f1592
GiveMe-A-Name <a455300764b@hotmail.com>
2021-05-28 18:00:37 +0800

    feat: update home-page

--- docs/.vuepress/components/Home/Body/components/Toolkit/index.vue
+++ docs/.vuepress/components/Home/Body/components/Toolkit/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <article class="toolbox-container">
-    <a name="tookit"></a>
-    <div class="toolbox-text">
-      <h3 class="title">Toolkit</h3>
-      <p class="descibe">一款多端开发的工具箱，以帮助开发者更快速配置前端环境，更轻松管理开发软件。</p>
-      <quick-start />
-      <nav-links class="toolbox-nav" :navItems="toolboxNavItems" />
+  <article class="toolkit-container">
+    <a name="toolkit" id="toolkit"></a>
+    <div class="toolkit-wrapper">
+      <div class="toolkit-text">
+        <h3 class="title">Toolkit</h3>
+        <p class="descibe">一款多端开发的工具箱，以帮助开发者更快速配置前端环境，更轻松管理开发软件。</p>
+        <quick-start />
+        <nav-links class="toolkit-nav" :navItems="toolkitNavItems" />
+      </div>
+      <img :src="require('@assets/Home/Body/Toolkit/toolkit-img.png')" alt="" class="toolkit-img" />
     </div>
-    <img :src="require('@assets/Home/Body/Toolkit/toolbox-img.png')" alt="" class="toolbox-img" />
   </article>
 </template>
 
@@ -22,10 +24,10 @@ export default {
   },
   data() {
     return {
-      toolboxNavItems: [
-        { imgSource: require('@assets/Home/Body/Toolkit/toolbox-icon1.png'), linkSource: '', text: '环境配置' },
+      toolkitNavItems: [
+        { imgSource: require('@assets/Home/Body/Toolkit/toolkit-icon1.png'), linkSource: '', text: '环境配置' },
         {
-          imgSource: require('@assets/Home/Body/Toolkit/toolbox-icon2.png'),
+          imgSource: require('@assets/Home/Body/Toolkit/toolkit-icon2.png'),
           linkSource: '',
           text: '软件管理',
         },
