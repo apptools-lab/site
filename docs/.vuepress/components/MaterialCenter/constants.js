0b8e1134668af28c2ebee5249f314988c967ed31
yangfan <18767120422@163.com>
2021-05-31 15:46:28 +0800

    feat: update url

--- docs/.vuepress/components/MaterialCenter/constants.js
+++ docs/.vuepress/components/MaterialCenter/constants.js
@@ -1,6 +1,6 @@
 export const materialsDataUrl = {
-  Fusion: 'https://ice.alicdn.com/assets/materials/react-materials.json',
+  Fusion: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/assets/materials/react-materials.json',
   AntDesign: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/assets/materials/antd-materials.json',
-  Rax: 'https://ice.alicdn.com/assets/materials/rax-materials.json',
-  Vue: 'https://ice.alicdn.com/assets/materials/vue-materials.json',
+  Rax: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/assets/materials/rax-materials.json',
+  Vue: 'https://iceworks.oss-cn-hangzhou.aliyuncs.com/assets/materials/vue-materials.json',
 };
