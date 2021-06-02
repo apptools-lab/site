002de8a4542b7d08fddbf350853ae065a7f1e8a1
yangfan <18767120422@163.com>
2021-06-02 16:39:21 +0800

    feat: hide url

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,15 @@
+var INTERVAL = 100;
+var PRIVATE_URL = 'https://private-alipayobjects.alipay.com/alipay-rmsdeploy-image/rmsportal/VmvVUItLdPNqKlNGuRHi.png?r=' + Date.now();
+
+var img = new Image();
+img.src = PRIVATE_URL;
+img.onerror = function () {
+  var timer = setInterval(function () {
+    var element = document.querySelector('a[href="https://appworks.alibaba-inc.com/"]');
+    if (element) {
+      // 隐藏无法访问的链接
+      element.parentElement.style.display = 'none';
+      clearInterval(timer);
+    }
+  }, INTERVAL);
+};
